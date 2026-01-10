3ceeee2ce8b084deb33aa429e1426477258bb547
harveymannering <harveymannering@hotmail.com>
2026-01-10 17:48:24 +0000

    Initial dataset

--- generate_images_flux.py
+++ generate_images_flux.py
@@ -31,31 +31,7 @@ import torch
 import cv2
 import numpy as np
 from sklearn.mixture import GaussianMixture
-
-
-def load_sdxl(args):
-    
-    # Create the DDIM noise schedule
-    noise_scheduler = DDIMScheduler(
-        num_train_timesteps=1000,
-        beta_start=0.00085,
-        beta_end=0.012,
-        beta_schedule="scaled_linear",
-        clip_sample=False,
-        set_alpha_to_one=False,
-        steps_offset=1,
-    )
-    
-    # Initialize SDXL
-    pipe = DiffusionPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0", # "SG161222/RealVisXL_V3.0", #"stabilityai/stable-diffusion-xl-base-1.0", 
-        torch_dtype=torch.float16,
-        scheduler=noise_scheduler,
-        use_safetensors=True,
-        variant="fp16",
-        cache_dir = args.cache_dir,
-    )
-    return pipe
+from diffusers import FluxKontextPipeline
 
 def set_seed(seed: int = 42):
     random.seed(seed)
@@ -78,7 +54,13 @@ if __name__ == '__main__':
     
     # Initialize the generator network
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    pipe = load_sdxl(args).to(device)
+    pipe = FluxKontextPipeline.from_pretrained(
+        "black-forest-labs/FLUX.1-Kontext-dev", 
+        torch_dtype=torch.bfloat16,
+        cache_dir = args.cache_dir
+    )
+    pipe.to(device)
+
     total_images = 100
     prompts = [
         "a portrait photo of a unattractive face",
@@ -89,14 +71,14 @@ if __name__ == '__main__':
     # Iterate over paths and generate random expressions
     for img_idx in range(total_images):
         with torch.no_grad():
-            noise = torch.randn((1, 4, 128, 128)).to(device)
-            noise = noise.repeat(len(prompts), 1, 1, 1)
+            latent = torch.randn(1, 4096, 64, device=pipe.transformer.device)
+            latent = latent.repeat(len(prompts), 1, 1)
             images = pipe(
-                latents=noise.half(),
+                latents=latent,
                 prompt=prompts,
-                #num_inference_steps=100,
+                num_inference_steps=20,
             ).images
-            
+
             # Save images
             for p_idx, img in enumerate(images):
                 if p_idx == 0:
